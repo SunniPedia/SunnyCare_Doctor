8748d61a0fa3d3df9d580387b6100a3f3114a8b2
SunniPedia <sunnipedia0@gmail.com>
2026-09-12 16:06:44 +0600

    Update SignupActivity.kt

--- app/src/main/kotlin/com/konasl/nagad/SignupActivity.kt
+++ app/src/main/kotlin/com/konasl/nagad/SignupActivity.kt
@@ -19,6 +19,7 @@ import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
 import android.view.Window
 import android.view.WindowManager
 import android.view.inputmethod.EditorInfo
