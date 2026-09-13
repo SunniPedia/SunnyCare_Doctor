bba4f3b51a5c45d71b6fb2c04b663a5977510338
SunniPedia <sunnipedia0@gmail.com>
2026-09-13 13:11:26 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.konasl.nagad"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
     }
 
     buildTypes {
