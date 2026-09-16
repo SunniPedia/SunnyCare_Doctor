d487205c0f0e94a172517830ba46ab9632c4d94e
SunniPedia <sunnipedia0@gmail.com>
2026-09-17 00:25:00 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,9 @@ buildscript {
         google()
         mavenCentral()
     }
+
     dependencies {
-        // আধুনিক অ্যান্ড্রয়েড বিল্ডের জন্য স্থিতিশীল ভার্সন
-        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("com.android.tools.build:gradle:8.9.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
